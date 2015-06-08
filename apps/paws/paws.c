63e63089349d788618df64f2833eb503c2f94b97
hobbes <robertgif@gmail.com>
2015-06-08 15:52:39 -0400

    Added apps

---
+++ apps/paws/paws.c
@@ -0,0 +1,34 @@
+#include <rump/rump_syscalls.h>
+#include <rump/rump.h>
+
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int
+main()
+{
+  int rv;
+
+  rv = rump_init();
+
+  if (rv)
+    printf("rump_init failed\n");
+
+  rv = rump_pub_etfs_register("/dev/paws", "paws", RUMP_ETFS_BLK);
+
+  if (rv)
+    printf("rump_pub_etfs_register failed\n");
+
+
+  DIR *dip = opendir("/dev");
+  struct dirent *dp;
+  while((dp = readdir(dip)) != NULL)
+  {
+    sleep(1);
+    printf("Current directory /dev/%s\n", dp->d_name);
+  }
+
+	return 0;
+}

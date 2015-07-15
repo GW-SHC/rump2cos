fad1610e9dffc819bf9086acb7e0a14564d07187
RobertGiff <robertgif@gmail.com>
2015-07-15 18:47:09 -0400

    added cos app and new rumprun updates

---
+++ apps/cos/cos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void
+__cerror(void)
+{
+	return;
+}
+
+void
+main(void)
+{
+	printf("Hello? Anyone out there?\n");
+	return;
+}
---
+++ apps/cos/rumpprint.c
@@ -0,0 +1,5 @@
+void
+rump_print()
+{
+        return;
+}

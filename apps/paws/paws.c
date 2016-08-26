cc99f77bee61492c59ed80b363dbc14b2e6131ba
lab176 <lbaier176@gmail.com>
2016-08-26 09:57:08 -0400

    marged local into master

--- apps/paws/paws.c
+++ apps/paws/paws.c
@@ -1,15 +1,17 @@
 #include "include/paws.h"
-#include <netconfig.h>
 #include <assert.h>
 #include <stdio.h>
 #include <malloc.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
-#include <fcntl.h>
-#include <net/if_cnic.h>
+#include "../../rumprun/src-netbsd/sys/net/if_cnic.h"
+#include <net/if.h>
+#include <net/route.h>
+#include <netinet/in.h>
+#include <netconfig.h>
 #include <errno.h>
-
+#include <fcntl.h>
 extern int vmid;
 
 int
